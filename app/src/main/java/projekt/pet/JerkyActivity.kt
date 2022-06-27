8aba4dab423770d77c19419798fd584764fa738d
Alina Prokofeva <whitestring@inbox.ru>
2022-06-27 23:53:20 +0300

    future ammount

--- app/src/main/java/projekt/pet/JerkyActivity.kt
+++ app/src/main/java/projekt/pet/JerkyActivity.kt
@@ -1,21 +1,29 @@
 package projekt.pet
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import projekt.pet.databinding.ActivityJerkyBinding
 import projekt.pet.databinding.ActivityJerkyBinding.inflate
+import java.math.RoundingMode
 
 class JerkyActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding  = ActivityJerkyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val jerky = jerky("Маринад для джерок (г/кг)",90.0,42.0,
-                    16.0,40.0, 5.0,2.0,20.0,8.0,
-            14.0,3.3,0.6)
+        val x = binding.editTextNumberDecimal.text.toString()
+        val y = x.toBigDecimal().setScale(2, RoundingMode.DOWN).toDouble()
+
+        val jerky = jerky("Маринад для джерок (г)",90.0*y,42.0*y,
+                    16.0*y,40.0*y, 5.0*y,2.0*y,20.0*y,8.0*y,
+            14.0*y,3.3*y,0.6*y)
 
         binding.render(jerky)
+
 
     }
 
